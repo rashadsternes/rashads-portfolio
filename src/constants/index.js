6ea424b5c0d48dff08e8edbc4152f01755d6e278
rashadsternes <nrsternes@gmail.com>
2025-07-02 13:52:03 -0500

    Add constants

---
+++ src/constants/index.js
@@ -0,0 +1,30 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Pulse Smartwatch",
+    description: "A beautiful landing page for the fictional 'Pulse Smartwatch' built using React and Tailwind.",
+    image: "/projects/pulse.png",
+    tags: ["React", "TailwindCSS", "Supabase"],
+    demoUrl: "https://pulse-smartwatch-tailwindcss.vercel.app/",
+    githubUrl: "https://github.com/rashadsternes/pulse-smartwatch-tailwindcss",
+  },
+  {
+    id: 2,
+    title: "Modern Movie App",
+    description:
+      "A movie finding site that where you can search and find new movies.",
+    image: "/projects/modern-movie-app.png",
+    tags: ["React", "TailwindCSS", "Database"],
+    demoUrl: "https://modern-movie-app.com/",
+    githubUrl: "https://github.com/rashadsternes/modern-movie-app",
+  },
+  {
+    id: 3,
+    title: "Photo Album App",
+    description: "An online photo album.",
+    image: "/projects/photo-album.png",
+    tags: ["React", "Material UI", "API"],
+    demoUrl: "https://photo-album-material-ui.com/",
+    githubUrl: "https://github.com/rashadsternes/photo-album-material-ui",
+  },
+];
