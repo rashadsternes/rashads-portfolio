f1e1f854b3a0de8048e4c7cddee4afc4d4c4a00f
rashadsternes <nrsternes@gmail.com>
2025-07-02 13:59:23 -0500

    Add Navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,9 @@
+import { Navbar } from "../components/Navbar";
+
 export const Home = () => {
   return (
     <div className="bg-background text-foreground ">
-        <h1 className="text-4xl font-bold mb-4">Welcome to My Portfolio</h1>
+      <Navbar />
     </div>
   );
 };
