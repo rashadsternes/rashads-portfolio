5828ac381ca60d5113083685725561e198c86170
rashadsternes <nrsternes@gmail.com>
2025-07-02 13:46:07 -0500

    First render 'Welcome to my portfolio'

---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+  return (
+    <div className="bg-background text-foreground ">
+        <h1 className="text-4xl font-bold mb-4">Welcome to My Portfolio</h1>
+    </div>
+  );
+};
