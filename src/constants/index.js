fcd08cf257c2f2e011a920dd0a90f9fbaebb9f26
rashadsternes <nrsternes@gmail.com>
2026-01-06 09:23:51 -0600

    Add the Kingdom Movement meal prep project

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,30 +1,30 @@
 export const projects = [
   {
     id: 1,
+    title: "Kingdom Movement",
+    description:
+      "An e-commerce meal delivery platform with custom-built ordering logic built for an actual client.",
+    image: "/kngdmmvmt.png",
+    tags: ["NextJS", "Sanity CMS", "NextAuth.js"],
+    demoUrl: "https://www.kngdmmvmt.com/",
+  },
+  {
+    id: 2,
     title: "Pulse Smartwatch",
-    description: "A beautiful landing page for the fictional 'Pulse Smartwatch' built using React and Tailwind.",
+    description:
+      "A beautiful landing page for the fictional 'Pulse Smartwatch' built using React and Tailwind.",
     image: "/pulse.png",
     tags: ["React", "TailwindCSS", "Supabase"],
     demoUrl: "https://pulse-smartwatch-tailwindcss.vercel.app/",
     githubUrl: "https://github.com/rashadsternes/pulse-smartwatch-tailwindcss",
   },
   {
-    id: 2,
+    id: 3,
     title: "Modern Movie App",
-    description:
-      "A movie finding site that where you can search and find new movies.",
+    description: "A movie finding site that where you can search and find new movies.",
     image: "/modern-movie-app.png",
     tags: ["React", "TailwindCSS", "Database"],
     demoUrl: "https://modern-movie-app.com/",
     githubUrl: "https://github.com/rashadsternes/modern-movie-app",
-  },
-  {
-    id: 3,
-    title: "Photo Album App",
-    description: "An online photo album.",
-    image: "/photo-album.png",
-    tags: ["React", "Material UI", "API"],
-    demoUrl: "https://photo-album-material-ui.com/",
-    githubUrl: "https://github.com/rashadsternes/photo-album-material-ui",
   },
 ];
